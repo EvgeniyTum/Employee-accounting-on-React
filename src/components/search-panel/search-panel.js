881d27f7b6139458e23da172cb1d0cf2a460e2bb
EvgeniyTum <gotic@list.ru>
2023-05-16 17:27:04 +0300

    Creating a project to register employees on React. Connecting the font-awesome and bootstrap libraries. Added components app-filter, app-info, employers-addform, employers-list, employers-list-item, search-panel. Added basic styles.

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-panel.css';
+
+const SeachPanel = () => {
+  return (
+    <input type="text"
+    className="form-control search-input sear"
+    placeholder="Найти сотрудника"/>
+  )
+}
+
+export default SeachPanel;
